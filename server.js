63255b5ab186ad97f2587d2ba0a7b025030f0f03
Wendy <wen.mcnair@gmail.com>
2024-04-25 11:25:33 -0400

    added get customer by id handler

--- server.js
+++ server.js
@@ -82,6 +82,20 @@ app.post('/customers', async (req, res) => {
 });
 
 
+// GET customer by id
+app.get("/customers/:id", async (req, res) => {
+    const id = req.params.id;
+    // return array [customer, errMessage]
+    const [cust, err] = await da.getCustomerById(id);
+    if(cust){
+        res.send(cust);
+    }else{
+        res.status(404);
+        res.send(err);
+    }   
+});
+
+
 
 
 
